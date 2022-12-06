1be21982e03780c0283b837274e5093aac437519
Networkspore <networkspore@gmail.com>
2022-12-05 23:28:52 -0600

    file caching

--- src/constants/utility.jsx
+++ src/constants/utility.jsx
@@ -112,7 +112,68 @@ export async function shuffle(array, seedStr) {
 
     return array;
 }
+export const crc32FromArrayBufferAsync = (ab) => {
+    return new Promise(resolve =>{
+        var table = new Uint32Array([
+            0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
+            0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
+            0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
+            0xf3b97148, 0x84be41de, 0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+            0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec, 0x14015c4f, 0x63066cd9,
+            0xfa0f3d63, 0x8d080df5, 0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
+            0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b, 0x35b5a8fa, 0x42b2986c,
+            0xdbbbc9d6, 0xacbcf940, 0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+            0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116, 0x21b4f4b5, 0x56b3c423,
+            0xcfba9599, 0xb8bda50f, 0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
+            0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d, 0x76dc4190, 0x01db7106,
+            0x98d220bc, 0xefd5102a, 0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+            0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818, 0x7f6a0dbb, 0x086d3d2d,
+            0x91646c97, 0xe6635c01, 0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
+            0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457, 0x65b0d9c6, 0x12b7e950,
+            0x8bbeb8ea, 0xfcb9887c, 0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+            0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2, 0x4adfa541, 0x3dd895d7,
+            0xa4d1c46d, 0xd3d6f4fb, 0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
+            0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9, 0x5005713c, 0x270241aa,
+            0xbe0b1010, 0xc90c2086, 0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+            0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4, 0x59b33d17, 0x2eb40d81,
+            0xb7bd5c3b, 0xc0ba6cad, 0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
+            0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683, 0xe3630b12, 0x94643b84,
+            0x0d6d6a3e, 0x7a6a5aa8, 0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+            0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe, 0xf762575d, 0x806567cb,
+            0x196c3671, 0x6e6b06e7, 0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
+            0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5, 0xd6d6a3e8, 0xa1d1937e,
+            0x38d8c2c4, 0x4fdff252, 0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+            0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60, 0xdf60efc3, 0xa867df55,
+            0x316e8eef, 0x4669be79, 0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
+            0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f, 0xc5ba3bbe, 0xb2bd0b28,
+            0x2bb45a92, 0x5cb36a04, 0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+            0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a, 0x9c0906a9, 0xeb0e363f,
+            0x72076785, 0x05005713, 0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
+            0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21, 0x86d3d2d4, 0xf1d4e242,
+            0x68ddb3f8, 0x1fda836e, 0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+            0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c, 0x8f659eff, 0xf862ae69,
+            0x616bffd3, 0x166ccf45, 0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
+            0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db, 0xaed16a4a, 0xd9d65adc,
+            0x40df0b66, 0x37d83bf0, 0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+            0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693,
+            0x54de5729, 0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
+            0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
+        ]);
 
+
+        var dsArr = new Uint8Array(ab);
+
+        var crc = 0 ^ (-1);
+
+        // var dsArr = ds.readUint8Array();
+        for (var i = 0; i < ab.byteLength; i++) {
+
+            crc = (crc >>> 8) ^ table[(crc ^ dsArr[i]) & 0xFF];
+        }
+        resolve(((crc ^ (-1)) >>> 0).toString(16))
+    
+    })
+}
 
 export const crc32FromArrayBuffer = (ab, callback) => {
 
@@ -226,6 +287,63 @@ export function formatedNow(now = new Date(), small = false) {
     return small ? stringNow : stringNow + ":" + stringSeconds + ":" + stringMiliseconds;
 }
 
+export async function getFirstDirectoryAllFiles(dirHandle) {
+    let files = []
+    let directories = []
+
+
+    await getDirectoryAllFiles(dirHandle, (file) => {
+        files.push(file)
+    }, (d) => {
+        directories.push(d)
+    })
+
+    return { files: files, directories: directories }
+
+}
+
+const allFileTypes = ["webp", "apng", "avif", "gif", "jpg", "jpeg", "jfif", "pjpeg", "pjp", "png", "svg", "svg", "bmp", "ico", "cur",
+"json", "obj", "fbx", "gltf", "glb", "dae", "babylon", "stl", "ply", "vrml",
+"pcm", "mp3", "ogg", "webm", "aac", "wav", "3gp", "avi", "mov", "mp4", "m4v", "m4a", "mkv", "ogv", "ogm", ".oga",]
+
+export async function getDirectoryAllFiles(dirHandle, pushFile, pushDirectory) {
+
+    const push = pushFile;
+    const pushDir = pushDirectory;
+
+    for await (const entry of dirHandle.values()) {
+
+        if (entry.kind === "file") {
+            const name = entry.name + "";
+
+            const lastIndex = name.lastIndexOf(".");
+
+            const ext = lastIndex != -1 ? lastIndex + 1 == name.length ? "" : name.slice(lastIndex + 1) : null
+
+            const typeIndex = allFileTypes.findIndex(fileTypes => fileTypes == ext)
+
+            const newFile = typeIndex != -1 ? await getAllFileInfo(entry, dirHandle) : null
+
+            if (newFile != null) {
+                push(newFile)
+            }
+
+
+        } else if (entry.kind == 'directory') {
+            pushDir(entry)
+            await getDirectoryAllFiles(entry, push, pushDir).then((result) => {
+
+            })
+        }
+
+    }
+    return true;
+
+}
+
+
+
+
 export async function getFirstDirectoryFiles(dirHandle, type, fileTypes = {}) {
     let files = []
     let directories = []
@@ -276,6 +394,41 @@ export async function getDirectoryFiles(dirHandle, type, fileTypes = [], pushFil
 }
 
 
+export async function getAllFileInfo(entry, dirHandle) {
+   
+    return new Promise(resolve => {
+     
+        entry.getFile().then((file) => {
+            
+            const firstIndex = file.type.indexOf("/")
+
+            const fileType = firstIndex == -1 ? file.type : file.type.slice(0,firstIndex + 1)
+
+            console.log(fileType)
+
+            file.arrayBuffer().then((arrayBuffer) => {
+
+                crc32FromArrayBuffer(arrayBuffer, (crc) => {
+
+                    get(crc + ".arcicon").then((iconInIDB) => {
+                        if (iconInIDB == undefined && type == "image") {
+                            getThumnailFile(file).then((dataUrl) => {
+                                set(crc + ".arcicon", dataUrl)
+                            }).catch((err) => console.log(err))
+                        }
+                    }).catch((err) => {
+                        console.log(err)
+                    })
+
+                    resolve({ directory: dirHandle, mimeType: fileType, name: file.name, crc: crc, size: file.size, type: file.type, lastModified: file.lastModified, handle: entry })
+                })
+            })
+
+            
+        })
+       
+    })
+}
 export async function getFileInfo(entry, dirHandle, type) {
 
     return new Promise(resolve => {
@@ -323,6 +476,57 @@ export async function getJsonFile(fileHandle){
 
     return config;
 
+}
+
+export async function cacheFile(localDirectory, file) {
+    try {
+        const fileName = `(${file.crc}) ${file.name}`  ;
+        const fileData = file.data
+
+        const cacheHandle = await localDirectory.getDirectoryHandle("cache", {create:true})
+
+        const newHandle = await writeFileData(cacheHandle, fileName, fileData)
+
+        const fileinfo = await getAllFileInfo(newHandle, cacheHandle)
+
+        return fileinfo
+
+    } catch (err) {
+        console.log(err)
+        return undefined;
+    }
+}
+
+export async function writeFileData(dirHandle, name, data){
+    try{
+        const fileHandle = await dirHandle.getFileHandle(name, { create: true })
+
+        const fileStream = await fileHandle.createWritable()
+
+        await fileStream.write(data)
+
+        await fileStream.close()
+
+        const newFileHandle = dirHandle.getFileHandle(name)
+
+        return newFileHandle
+
+    }catch(err){
+        console.log(err)
+
+        return undefined
+    }
+}
+
+export async function getFileData(localFile){
+    const file = await localFile.handle.getFile()
+
+    const data = await file.arrayBuffer()
+
+    const crc = await crc32FromArrayBufferAsync(data)
+
+    if (crc != localFile.crc) throw new Error("File corrupt")
+    return data;
 }
 
 export async function getImageHandleDataURL(localFile){
@@ -388,33 +592,38 @@ export async function getThumnailFile(file, size = { width: 100, height: 100 }) 
 
 }
 
-export const genID = () =>{
-    randInt
-}
 
-export const generateCode = (words) =>{
-     return new Promise(resolve =>{
-        
-        let word = WordArray.random(randInt(20, 30)).toString()
+export async function generateCode(words){
+    
+       
+    let word = ""
+    
+    if(Array.isArray(words))
+    {
+        words.forEach(element => {
+            word.concat(element);
+        });
+    }else{
+        word = words + ""
+    }
+    
+    const randomNumber1 = await getRandomInt(10, 20, word)
 
-        
-        if(Array.isArray(words))
-        {
-            words.forEach(element => {
-                word += element;
-            });
-        }else{
-            word += words.toString()
-        }
+    word.concat(WordArray.random(randomNumber1).toString())
 
-        
-        const salt = SHA512(word).toString().slice(randInt(0, 10), randInt(15, 30));
+    const randomNumber2 = await getRandomInt(0, 15, word)
+    const randomNumber3 = await getRandomInt(20, 30, word)
 
-        const time = formatedNow(new Date(), true).slice(randInt(0, 10))
-        const string = time + salt;
-        const md5 = MD5(string)
-        resolve(md5.toString())
-    })
+    const code1 = SHA512(word).toString().slice(randomNumber2, randomNumber3);
+
+    const randomNumber4 = await getRandomInt(10, 15, code1)
+
+    const code2 = SHA512(formatedNow()).toString().slice(randomNumber2, randomNumber4)
+
+    const result = code1 + code2
+    console.log(result)
+    return result
+    
 }
 
 export async function getPermissionAsync(handle){
